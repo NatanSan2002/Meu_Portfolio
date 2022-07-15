529f3456d1a375e462ebf00ed8b10b1fa98a9270
Natan2002 <102757932+NatanSan2002@users.noreply.github.com>
2022-07-14 21:41:28 -0300

    14/07/22

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll",function(e){
+if(this.scrollY>0){
+document.getElementById("cabecalho").setAttribute("class","roll");
+
+document.getElementById("header_icon").setAttribute("class","header_icon_small");
+}else{
+    document.getElementById("cabecalho").removeAttribute("class","roll");
+
+    
+
+} 
+})
