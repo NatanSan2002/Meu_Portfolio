8820478873a86d4eb00455245d543f96e5a5cdaf
Natan2002 <102757932+NatanSan2002@users.noreply.github.com>
2022-07-18 22:53:45 -0300

    18/07/22

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+/*Roll Sense: Change Menu Bar*/
+
 window.addEventListener("scroll",function(e){
 if(this.scrollY>0){
 document.getElementById("cabecalho").setAttribute("class","roll");
@@ -10,3 +13,6 @@ document.getElementById("header_icon").setAttribute("class","header_icon_small")
 
 } 
 })
+
+
+
