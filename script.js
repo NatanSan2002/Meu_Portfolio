fe20c5387704d04bc2d8786a4a00c7e01a91d92e
Natan2002 <102757932+NatanSan2002@users.noreply.github.com>
2022-07-20 20:43:21 -0300

    Começando Menu Mobile

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 
 /*Roll Sense: Change Menu Bar*/
 
+
 window.addEventListener("scroll",function(e){
-if(this.scrollY>0){
+if(this.scrollY > 0 && window.screen.width > 950){
 document.getElementById("cabecalho").setAttribute("class","roll");
 
 document.getElementById("header_icon").setAttribute("class","header_icon_small");
 }else{
     document.getElementById("cabecalho").removeAttribute("class","roll");
-
-    
-
 } 
 })
+
 
 
 
