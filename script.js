030df4818e0df624c86e344aa1755f87fb31d905
Natan2002 <102757932+NatanSan2002@users.noreply.github.com>
2022-07-21 20:59:39 -0300

    Menu Mobile Finalizado e correções

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
 
+// Var Globais 
+
+let header = document.getElementById("cabecalho");
+let icon_header = document.getElementById("header_icon");
+
+
+
 /*Roll Sense: Change Menu Bar*/
 
 
 window.addEventListener("scroll",function(e){
 if(this.scrollY > 0 && window.screen.width > 950){
-document.getElementById("cabecalho").setAttribute("class","roll");
+header.setAttribute("class","roll");
 
-document.getElementById("header_icon").setAttribute("class","header_icon_small");
+icon_header.setAttribute("class","header_icon_small");
 }else{
-    document.getElementById("cabecalho").removeAttribute("class","roll");
+    header.removeAttribute("class","roll");
 } 
 })
 
+
+// Mobile Menu
+
+let exb_menu = false;
+
+
+function Act_MobMenu(){
+if(exb_menu == false){
+
+header.style.display = "flex";
+exb_menu = true;
+
+}else{
+    header.style.display = "none";
+    exb_menu = false;
+}
+}
+
+window.addEventListener('resize',function(){
+let largura = window.screen.width;
+
+if(largura > 950){
+    header.style.display = "flex";
+    }else if(largura < 950 && exb_menu == false){
+        header.style.display = "none";
+    }
+})
 
 
 
